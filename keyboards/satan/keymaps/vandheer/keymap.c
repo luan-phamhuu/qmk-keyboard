f8536b736a8877ebcd39bafc63d5386eda68ce29
Luan Pham <phluan92@gmail.com>
2016-09-24 20:29:19 +0700

    open Force Quit Applications with Function Layer

--- keyboards/satan/keymaps/vandheer/keymap.c
+++ keyboards/satan/keymaps/vandheer/keymap.c
@@ -53,7 +53,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_PGDN,  KC_NO,    KC_NO,    KC_NO,    KC_NO,                    KC_TRNS,\
   KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,                         KC_TRNS,  KC_TRNS,KC_TRNS,          KC_TRNS),
 
-
 /* Keymap _FL: Function Layer
    * ,-----------------------------------------------------------.
    * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|  DEL  |
@@ -68,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_FL] = KEYMAP_ANSI(
-  KC_GRV,   KC_F1,    KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12, KC_DEL, \
+  LALT(LGUI(KC_ESC)),   KC_F1,    KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12, KC_DEL, \
   KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,  KC_NO,  \
   KC_TRNS,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_VOLD,  KC_VOLU,          KC_TRNS,\
   KC_TRNS,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,                      KC_TRNS,\
