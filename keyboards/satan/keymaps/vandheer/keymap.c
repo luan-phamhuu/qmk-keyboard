56501d9853e165103836cb3ee1476f8891a66238
LuanPham <luan.pham@quoine.com>
2017-12-21 13:36:49 +0700

    change navigationn arror to vim style, remove atom macro

--- keyboards/satan/keymaps/vandheer/keymap.c
+++ keyboards/satan/keymaps/vandheer/keymap.c
@@ -1,7 +1,7 @@
 #include "satan.h"
 
 // Used for SHIFT_ESC
-#define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
+#define MODS_CTRL_MASK (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -9,11 +9,10 @@
 // entirely and just use numbers.
 #define _BL 0 // Base Layer
 #define _NA 1 // Navigation Layer
-#define _FL 2 // Function Layer
+#define _FN 2 // Function Layer
 #define _MA 3 // Macro layer
 
 // Define name of macro
-#define ATOM M(0)
 #define CHROME M(1)
 #define SLACK M(2)
 #define MAIL M(3)
@@ -32,11 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
    * |-----------------------------------------------------------|
-   * |Nav~Esc|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |Nav    |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
    * |-----------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
    * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |      Space            |Gui |Alt |FN  |Macro|
+   * |Ctrl|Alt |Gui |      Space            |Cap |Alt |FN  |Macro|
    * `-----------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
@@ -44,59 +43,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
   MO(_NA), KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
   KC_LSPO, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,                   KC_RSPC, \
-  KC_LCTL, KC_LALT, KC_LGUI,                           KC_SPC,                             KC_CAPS, KC_RALT, MO(_FL), MO(_MA)),
+  KC_LCTL, KC_LALT, KC_LGUI,                           KC_SPC,                             KC_CAPS, KC_RALT, MO(_FN), MO(_MA)),
 
 /* Keymap _NA: Navigation Layer
    * ,-----------------------------------------------------------.
    * | ` |   |   |   |   |   |   |   |   |   |   |   |   |       |
    * |-----------------------------------------------------------|
-   * |     |   |   |   |   |   |   |Hom|Up |End|   |   |   |     |
+   * |     |   |   |   |   |   |Hom|PDo|PUp|End|   |   |   |     |
    * |-----------------------------------------------------------|
-   * |Nav~Esc|   |   |   |   |   |PUp|Lef|Dow|Rig|   |   |       |
+   * |Nav    |   |   |   |   |   |Lef|Dow|Up |Rig|   |   |       |
    * |-----------------------------------------------------------|
-   * |Shift   |   |   |   |   |   |PDo|   |   |   |   |          |
+   * |Shift   |   |   |   |   |   |   |   |   |   |   |          |
    * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |                        |Gui |Alt |Fn |Macro|
+   * |Ctrl|Alt |Gui |                        |Cap |Alt |Fn |Macro|
    * `-----------------------------------------------------------'
    */
 [_NA] = KEYMAP_ANSI(
   KC_GRV,   KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO, \
-  KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  ITERM,  KC_NO,    KC_HOME,  KC_UP,    KC_END,   PMAN,    KC_NO,  KC_NO,  KC_NO,  \
-  KC_TRNS,  ATOM,    SLACK,  KC_NO,  KC_NO,  KC_NO,  KC_PGUP,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_NO,    KC_NO,          KC_TRNS,\
-  KC_TRNS,  KC_NO,    KC_NO,  CHROME,  KC_NO,  KC_NO,  KC_PGDN,  MAIL,    KC_NO,    KC_NO,    KC_NO,                    KC_TRNS,\
+  KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  ITERM,  KC_HOME,  KC_PGUP,  KC_PGDN,  KC_END,   PMAN,     KC_NO,  KC_NO,  KC_NO,  \
+  KC_TRNS,  KC_NO,    SLACK,  KC_NO,  KC_NO,  KC_NO,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_NO,    KC_NO,          KC_TRNS,\
+  KC_TRNS,  KC_NO,    KC_NO,  CHROME, KC_NO,  KC_NO,  KC_NO,    MAIL,     KC_NO,    KC_NO,    KC_NO,                    KC_TRNS,\
   KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,                         KC_TRNS,  KC_TRNS,KC_TRNS,          KC_TRNS),
 
-/* Keymap _FL: Function Layer
+/* Keymap _FN: Function Layer
    * ,-----------------------------------------------------------.
    * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|  DEL  |
    * |-----------------------------------------------------------|
    * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
    * |-----------------------------------------------------------|
-   * |Nav~Esc|   |   |   |   |   |   |   |   |   |Vol-|Vol+|     |
+   * |Nav    |   |   |   |   |   |   |   |   |   |Vol-|Vol+|     |
    * |-----------------------------------------------------------|
    * |Shift   |   |   |   |   |   |   |   |   |   |   |          |
    * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |                        |Gui |Alt |Fn |Macro|
+   * |Ctrl|Alt |Gui |                        |Cap |Alt |Fn |Macro|
    * `-----------------------------------------------------------'
    */
-[_FL] = KEYMAP_ANSI(
+[_FN] = KEYMAP_ANSI(
   LALT(LGUI(KC_ESC)),   KC_F1,    KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12, KC_DEL, \
   KC_NO,    KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,    KC_NO,    KC_NO,  KC_NO,  \
   KC_TRNS,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, KC_VOLD,  KC_VOLU,          KC_TRNS,\
   KC_TRNS,  KC_NO,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,                      KC_TRNS,\
   KC_TRNS,  KC_TRNS,  KC_TRNS,                     KC_TRNS,                         KC_TRNS,  KC_TRNS,KC_TRNS,            KC_TRNS),
 
-  /* Keymap _BL: (Base Layer) Default Layer
+  /* Keymap _MA: Macro Layer
    * ,-----------------------------------------------------------.
-   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
+   * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |
    * |-----------------------------------------------------------|
-   * |     |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
+   * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
    * |-----------------------------------------------------------|
-   * |Nav~Esc|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |       |   |   |   |   |   |   |   |   |   |   |   |       |
    * |-----------------------------------------------------------|
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
+   * |        |   |   |   |   |   |   |   |   |   |   |          |
    * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |      Space            |Gui |Alt |FN  |Ctrl |
+   * |    |    |    |                        |    |    |   |Macro|
    * `-----------------------------------------------------------'
    */
 [_MA] = KEYMAP_ANSI(
