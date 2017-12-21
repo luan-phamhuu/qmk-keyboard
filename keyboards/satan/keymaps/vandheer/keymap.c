6221f84454607b63e1eb8807407a11e3e40294c9
LuanPham <luan.pham@quoine.com>
2017-12-21 14:31:35 +0700

    switch ctrl and navigation layer

--- keyboards/satan/keymaps/vandheer/keymap.c
+++ keyboards/satan/keymaps/vandheer/keymap.c
@@ -31,19 +31,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
    * |-----------------------------------------------------------|
-   * |Nav    |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |Ctrl   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
    * |-----------------------------------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
    * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |      Space            |Cap |Alt |FN  |Macro|
+   * |_NA |Alt |Gui |      Space            |Cap |Alt |_FN |Macro|
    * `-----------------------------------------------------------'
    */
 [_BL] = KEYMAP_ANSI(
   F(0),    KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-  MO(_NA), KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
+  KC_LCTL, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
   KC_LSPO, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT, KC_SLSH,                   KC_RSPC, \
-  KC_LCTL, KC_LALT, KC_LGUI,                           KC_SPC,                             KC_CAPS, KC_RALT, MO(_FN), MO(_MA)),
+  MO(_NA), KC_LALT, KC_LGUI,                           KC_SPC,                             KC_CAPS, KC_RALT, MO(_FN), MO(_MA)),
 
 /* Keymap _NA: Navigation Layer
    * ,-----------------------------------------------------------.
@@ -51,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |     |   |   |   |   |   |Hom|PDo|PUp|End|   |   |   |     |
    * |-----------------------------------------------------------|
-   * |Nav    |   |   |   |   |   |Lef|Dow|Up |Rig|   |   |       |
+   * |Ctrl   |   |   |   |   |   |Lef|Dow|Up |Rig|   |   |       |
    * |-----------------------------------------------------------|
    * |Shift   |   |   |   |   |   |   |   |   |   |   |          |
    * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |                        |Cap |Alt |Fn |Macro|
+   * |_NA |Alt |Gui |                       |Cap |Alt |_FN |Macro|
    * `-----------------------------------------------------------'
    */
 [_NA] = KEYMAP_ANSI(
@@ -71,11 +71,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
    * |-----------------------------------------------------------|
-   * |Nav    |   |   |   |   |   |   |   |   |   |Vol-|Vol+|     |
+   * |Ctrl   |   |   |   |   |   |   |   |   |   |Vol-|Vol+|     |
    * |-----------------------------------------------------------|
    * |Shift   |   |   |   |   |   |   |   |   |   |   |          |
    * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |                        |Cap |Alt |Fn |Macro|
+   * |_NA |Alt |Gui |                       |Cap |Alt |_FN |Macro|
    * `-----------------------------------------------------------'
    */
 [_FN] = KEYMAP_ANSI(
